dfcbf2fa4015aca0ec5e0fbc0924e60627b9c7f3
atkhssn <atik.hassan@outlook.com>
2019-11-11 13:19:17 +0600

    Solved the 8th problem in C language

---
+++ prob8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Write a C program to enter temperature in °Celsius  and convert it into °Fahrenheit >>
+
+int main ()
+{
+    float temperature, fahrenheit;
+
+    printf("Enter a value of temperature in Celsius: \n");
+    scanf("%f", &temperature);
+
+    fahrenheit = ((9 * temperature) + 160) / 5; // Law of °F to °C, °F = (9 * temperature °C +160) / 5
+
+    printf("The temperature you\'ve entered %.2f C and It\'s fahrenheit value is %.2f F \n", temperature, fahrenheit);
+    return 0;
+}
